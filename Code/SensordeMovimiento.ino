4b314f733650400631c329f5fd4c98de066859ae
Juan Sebastian Ruiz <47191714+JuanSeRuiz@users.noreply.github.com>
2019-02-19 20:34:21 -0500

    Add files via upload

--- Code/SensordeMovimiento.ino
+++ Code/SensordeMovimiento.ino
@@ -1,16 +1,49 @@
-int sensorPin = 2;
-int ledPin = 3;
- 
+const int trigPin =3;
+const int echoPin =2;
+int led1 =4;
+int led2 =5;
+int led3 =6;
+
+long duration;
+int distance;
+
 void setup() {
-pinMode(sensorPin, INPUT);
-pinMode(ledPin, OUTPUT);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+    Serial.begin(9600);
 }
 
-
 void loop() {
-  if(digitalRead(sensorPin) == HIGH){
-    digitalWrite(ledPin, HIGH);
-  } else{
-    digitalWrite(ledPin, LOW);
-  }
-  }
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  
+  duration = pulseIn(echoPin, HIGH);
+  
+  distance = 0.034 * duration / 2;
+  
+    Serial.print("distancia: ");
+    Serial.println(distance);
+    
+   if (distance < 25 ){
+
+ digitalWrite(led3,HIGH);
+ }else{
+ digitalWrite(led3, LOW);
+}
+
+  if(distance < 15 ){
+
+ digitalWrite(led2,HIGH);
+ }else{
+ digitalWrite(led2, LOW);
+}  if (distance < 5 ){
+
+ digitalWrite(led1,HIGH);
+ }else{
+ digitalWrite(led1, LOW);
+}
+}
